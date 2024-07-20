0721fc840c4c16b39b26ffb982890eed64c713f8
Vine Dube <pvdvine27@gmail.com>
2024-07-20 23:46:35 +0200

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+
+
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signout" element={<SignOut />} />
+        
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/pages/SignOut.jsx
@@ -0,0 +1,8 @@
+
+const SignOut = () => {
+  return (
+    <div>SignUp</div>
+  )
+}
+
+export default SignOut;
